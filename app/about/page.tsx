c057f35e6627596e4e1ede4f6eaacf4a9620da5a
Ajarek <ajarek@poczta.onet.pl>
2025-10-11 00:48:08 +0200

    add about

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import TeamSection from '@/components/team'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className=' flex flex-col items-center justify-center gap-8 pt-4'>
+      <TeamSection/>
+    </div>
+  )
+}
+
+export default About
---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className=' flex flex-col items-center justify-center gap-8 pt-4'>Contact</div>
+  )
+}
+
+export default Contact
